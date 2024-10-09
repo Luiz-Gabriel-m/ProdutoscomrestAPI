f526241f350dc708f116ad2666caa6fbe3a61914
Luiz Gabriel <lg17demoraes@gmail.com>
2024-10-09 11:58:44 -0300

    programa revisado 100% com comentarios

--- primeiraapi/src/main/java/com/example/primeiraapi/Controller.java
+++ primeiraapi/src/main/java/com/example/primeiraapi/Controller.java
@@ -5,27 +5,28 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 
 @RestController // esta linha e para declarar que esta classe e um controller
-@RequestMapping("/Loja")// e esta e para definir a uri de assesso deste controller
+@RequestMapping("/Loja")// e esta e para definir a uri/endpoint de assesso deste controller
 
 public class Controller {
 
-    ArrayList<Produto> lista_de_produtos = new ArrayList<Produto>();
+    ArrayList<Produto> lista_de_produtos = new ArrayList<Produto>();//Aqui criei meu arraylist e atribui um nome a ele
 
-    public Controller () {
+    public Controller() {//aqui criei meu construtor que adiciona meus produtos no meu arraylist atraves de outro metodo
 
-        adicionarprodutos();
-
-    }
-    @GetMapping("/Listarprodutos")
-    public ArrayList<Produto> listarprodutos() {
-
-        return lista_de_produtos;
+        adicionarprodutos();//aqui chamo o metodo que adiciona meus produtos na minha lista
 
     }
 
-    public void adicionarprodutos() {
+    @GetMapping("/Listarprodutos")//aqui criei um endpoint que chama o metodo listar produtos
+    public ArrayList<Produto> listarprodutos() {//aqui criei o metodo que lista meus produtos
 
-        Produto produto0 = new Produto(0, "Iphone 16 pro max", 11400.00, "Smartphones");
+        return lista_de_produtos;//aqui eu retorno minha lista de clientes apenas exibindo não adiciona ou remove nada
+
+    }
+
+    public void adicionarprodutos() {//aqui criei o metodo que adiciona os meus produtos
+
+        Produto produto0 = new Produto(0, "Iphone 16 pro max", 11400.00, "Smartphones");//aqui concatenei as infomacoes correspondemtes a cada produto
         Produto produto1 = new Produto(1, "Iphone 15 pro max", 5499.99, "Smartphones");
         Produto produto2 = new Produto(2, "Iphone 12 pro max", 3799.99, "Smartphones");
         Produto produto3 = new Produto(3, "Sansung S22 Ultra 5G", 3500.00, "Smartphones");
@@ -36,7 +37,7 @@ public class Controller {
         Produto produto8 = new Produto(8, "Batata Elma Chips Doritos 140g", 14.99, "Comidas");
         Produto produto9 = new Produto(9, "Água", 2.50, "Bebidas");
 
-        lista_de_produtos.add(produto0);
+        lista_de_produtos.add(produto0);//aqui adicionei na minha lista cada produto com as informações que concatenei anteriormente
         lista_de_produtos.add(produto1);
         lista_de_produtos.add(produto2);
         lista_de_produtos.add(produto3);
@@ -50,50 +51,58 @@ public class Controller {
     }
 
 
-    @GetMapping("/Quantidadetotaldeprodutos")
+    @GetMapping("/Quantidadetotaldeprodutos")//aqui criei um endpoint que chama o metodo que conta quantos produtos minha lista contem
 
-    public int quantidade() {
+    public int quantidade() {//aqui criei o metodo que conta quantos produtos minha lista contem
 
-        return lista_de_produtos.size();
-
-    }
-
-    @GetMapping("/Removertodososprodutos")
-
-    public String removertodalista() {
-
-        lista_de_produtos.clear();
-
-        return "Produtos removidos com sucesso!";
+        return lista_de_produtos.size();//aqui retorno minha lista porem com um parametro do arraylist que conta a quantidade de itens presentes na minha lista assim retornando o valor total de itens que a lista contem
 
     }
 
-    @GetMapping("/Readicionarosprodutos")
-    public String readicionarprodutos() {
+    @GetMapping("/Removertodososprodutos")//aqui criei um endpoint que chama o metodo que ira remover todos os produtos da minha lista
 
-        if (lista_de_produtos.isEmpty()) {
+    public String removertodalista() {//aqui criei o metodo que ira remover todos os produtos da minha lista porem ira retornar uma mensagem do tipo string informando o que aconteceu
 
-            adicionarprodutos();
 
-        } else {
+        lista_de_produtos.clear();//aqui retorno minha lista porem com um parametro do arraylist que remove todos os produtos da minha lista
 
-            System.out.println("Não foi possivel readicionar pois os itens não foram removidos!");
+        return "Produtos removidos com sucesso!";//aqui retorno uma menssagem do que foi realizado
+
+    }
+
+    @GetMapping("/Readicionarosprodutos")//aqui criei um endpoint que chama o metodo readicionar produtos
+    public String readicionarprodutos() {//aqui criei o metodo que ira readicionar todos os produtos da minha lista porem ira retornar uma mensagem do tipo string informando o que aconteceu
+
+        if (lista_de_produtos.isEmpty()) {//aqui criei um if para comparar se me lista esta vazia ou n pois ele so ira readicionar os produtos cas a lista esteja vazia caso contrario ele redireciona direto ao else
+
+            adicionarprodutos();//aqui chamo meu metodo que adiciona meus produtos
+
+        } else {//aqui exibe uma mensagem informando o que aconteceu
+
+            System.out.println("Não foi possivel readicionar pois os itens não foram removidos!");//aqui retorno uma menssagem do que foi realizado
 
         }
-        return "Produtos readicionados com sucesso!";
+        return "Produtos readicionados com sucesso!";//aqui retorno uma menssagem do que foi realizado
     }
 
-    @GetMapping("/Buscarid/{id}")
-    public Produto buscarid (@PathVariable int id) {
+    @GetMapping("/Buscarid/{id}")//aqui criei um endpoint com um parametro que ira chamar meu metodo que busca o produto que corresponde ao id desejado
+    public Produto buscarid(@PathVariable int id) {//aqui crei meu metodo do tipo Produto com o nome buscar id onde ele tem uma pathvariable que e um parametro do endpoint do tipo int que ira receber um valor
 
-        for (Produto produto : lista_de_produtos) {
-            if (produto.getId() == id) {
+        for (Produto produto : lista_de_produtos) {//aqui crei um for que ira percorrer minha lista ate achar um produto que tenha o id que foi informado no parmetro do enpoint e armazenar numa varivel temporaria
+            if (produto.getId() == id) {//aqui criei um if que pega o conteudo da variavel temporaria e compara com o valor irformado e se ele for igual ao informafo ele exibe o produto que tem o mesmo id do informado
 
-                return produto;
+                return produto;//aqui ele exibe o produto que tem o mesmo id do informado
 
             }
         }
-        return null;
+        return null;//aqui ele retorna nulo
+    }
+
+    @GetMapping("/Buscarid")//aqui criei um endpoint que chama o metodo mensagem
+    public String mensagem() {//aqui crei o metodo menssagem que retorna uma string
+
+        return "Você não informou o /'id' que deseja buscar!";//aqui retorno uma menssagem
+
     }
 }
 
--- primeiraapi/src/main/java/com/example/primeiraapi/Produto.java
+++ primeiraapi/src/main/java/com/example/primeiraapi/Produto.java
@@ -1,19 +1,19 @@
 package com.example.primeiraapi;
 
-public class Produto {
+public class Produto {//aqui criei minha classe Produto
 
-    private int Id;
+    private int Id;//aqui criei uma caracteristica do tipo int
 
-    private String nome;
+    private String nome;//aqui criei uma caracteristica do tipo String
 
-    private double preco;
+    private double preco;//aqui criei uma caracteristica do tipo double
 
-    private String categoria;
+    private String categoria;//aqui criei uma caracteristica do tipo String
 
 
-    public Produto (int ID, String Nome, double Preco, String Categoria) {
+    public Produto(int ID, String Nome, double Preco, String Categoria) {//aqui crei um metodo com varios parametros
 
-        this.Id = ID;
+        this.Id = ID;//aqui digo que minhas caracteristicas vão ser iguais as dos parametros do meu metodo
         this.nome = Nome;
         this.preco = Preco;
         this.categoria = Categoria;
@@ -22,7 +22,7 @@ public class Produto {
 
     public int getId() {
         return Id;
-    }
+    }//aqui eu fasso os gets das minhas caracteristicas
 
     public String getNome() {
         return nome;
